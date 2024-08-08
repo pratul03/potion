4dc5e353ff4364d6f66e36a777a92f198b561b4f
Pratul Makar <pratulmakar02@gmail.com>
2024-08-08 23:18:02 +0530

    updated

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,14 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
-export default function Home() {
+import { Heading } from "./_components/Heading";
+import { Heroes } from "./_components/Heroes";
+const MarketingPage = () => {
   return (
-    <main className="flex">
-      <Button className="bg-emerald-500 text-white">
-        Click here
-      </Button>
-    </main>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+    </div>
   );
-}
+};
+
+export default MarketingPage;
