56df10cbd2005aa5d2c7f375f380f820e13280ea
Pratul Makar <pratulmakar02@gmail.com>
2024-08-15 23:01:40 +0530

    new

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useUser } from "@clerk/clerk-react";
-import { Avatar } from "@radix-ui/react-avatar";
+import { SignOutButton, useUser } from "@clerk/clerk-react";
+import { ChevronsLeftRight } from "lucide-react";
 
 const UserItem = () => {
   const { user } = useUser();
@@ -26,8 +30,45 @@ const UserItem = () => {
               {user?.fullName}&apos;s Potion
             </span>
           </div>
+
+          <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
         </div>
       </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-80"
+        align="start"
+        alignOffset={11}
+        forceMount
+      >
+        <div className="flex flex-col space-y-4 p-2">
+          <p className="text-xs font-medium leading-none text-muted-foreground">
+            {user?.emailAddresses[0].emailAddress}
+          </p>
+          <div className="flex items-center gap-x-2">
+            <div className="rounded-md bg-secondary p-1">
+              <Avatar className="h-8 w-8">
+                <AvatarImage src={user?.imageUrl} />
+              </Avatar>
+            </div>
+            <div className="space-y-1">
+              <p className="text-sm line-clamp-1">
+                {user?.fullName}&apos;s Potion
+              </p>
+            </div>
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer w-full text-muted-foreground">
+          <SignOutButton>
+            <Button
+              variant="ghost"
+              className="hover:bg-red-600/30 rounded-md hover:text-red-500"
+            >
+              Log out
+            </Button>
+          </SignOutButton>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
