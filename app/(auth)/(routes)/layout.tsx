0c19be4d9d68cfae26351a1623289178c84fe27b
Pratul Makar <pratulmakar02@gmail.com>
2024-08-15 09:44:15 +0530

    new

--- app/(auth)/(routes)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-const RootLayout = ({ children }: { children: ReactNode }) => {
-    return (
-        <div className="h-full bg-emerald-400">{children}</div>
-    );
-};
-
-export default RootLayout;
