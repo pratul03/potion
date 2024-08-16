896c4666e1a2b7dd83045eb99ecfb4b5f122540c
Pratul Makar <pratulmakar02@gmail.com>
2024-08-16 20:43:39 +0530

    new

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -60,8 +60,11 @@ export const Item = ({
       )}
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded-md border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-          <span><Command size={10}/></span>K
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded-md border bg-muted px-1.5 font-mono text-[12px] font-semibold text-muted-foreground opacity-100">
+          <span>
+            <Command size={12} />
+          </span>
+          K
         </kbd>
       )}
     </div>
