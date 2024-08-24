bd7a7bc94db44c8417f1f3ad9ce955f6d06bb1ab
Pratul Makar <pratulmakar02@gmail.com>
2024-08-24 20:01:57 +0530

    new

--- components/modals/settings-modal.tsx
+++ components/modals/settings-modal.tsx
@@ -1,25 +1,19 @@
 "use client";
 
-import {
-    Dialog,
-    DialogContent,
-    DialogHeader
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader } from "@/components/ui/dialog";
 
 import { useSettings } from "@/hooks/use-settings";
-import { Label } from "@/components/ui/label";
-import { ModeToggle } from "@/components/mode-toggle";
 
 export const SettingsModal = () => {
-    const settings = useSettings();
+  const settings = useSettings();
 
-    return (
-        <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
-            <DialogContent>
-                <DialogHeader className="border-b pb-3">
-                    <h2 className="text-lg font-medium">My Settings</h2>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
-    )
-}
+  return (
+    <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
+      <DialogContent>
+        <DialogHeader className="border-b pb-3">
+          <h2 className="text-lg font-medium">My Settings</h2>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
