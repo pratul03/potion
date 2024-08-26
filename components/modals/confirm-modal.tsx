7636f48da00a34ca60c77ed6f8b536b733a379d5
Pratul Makar <pratulmakar02@gmail.com>
2024-08-26 09:21:28 +0530

    update ui

--- components/modals/confirm-modal.tsx
+++ components/modals/confirm-modal.tsx
@@ -40,13 +40,13 @@ export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
         <AlertDialogFooter>
           <AlertDialogCancel
             onClick={(e) => e.stopPropagation()}
-            className="hover:bg-blue-500/30 hover:text-blue-600"
+            className="hover:bg-blue-500/30 hover:text-blue-600 hover:font-semibold transition-all ease-linear duration-100"
           >
             Cancel
           </AlertDialogCancel>
           <AlertDialogAction
             onClick={handleConfirm}
-            className="outline outline-neutral-200 outline-1 bg-transparent text-black hover:text-red-600 hover:bg-red-400/40 dark:text-white dark:hover:text-red-600 dark:hover:bg-red-400 dark:hover:outline-none"
+            className="outline outline-neutral-200/10 outline-1 bg-transparent text-black hover:text-red-600 hover:bg-red-400/40 dark:text-white dark:hover:text-red-600 dark:hover:bg-red-400/20 dark:hover:outline-none hover:font-semibold transition-all ease-linear duration-100"
           >
             Confirm
           </AlertDialogAction>
