896f55f51b2f58a83a2e610ef5e66ec5a9c651c6
Pratul Makar <pratulmakar02@gmail.com>
2024-08-28 11:34:36 +0530

    new

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -6,7 +6,7 @@ import { Toolbar } from "@/components/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 interface DocumentIdProps {
   params: {
@@ -17,6 +17,15 @@ const DocumentIdPage = ({ params }: DocumentIdProps) => {
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
 
   if (document === undefined) {
     return (
@@ -42,10 +51,7 @@ const DocumentIdPage = ({ params }: DocumentIdProps) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
-        <Editor
-          onChange={() => { }} 
-          initialContent={document.content}
-        />
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   );
