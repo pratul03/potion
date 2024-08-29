149e248a62fe4735d2376f0f51e8b10e9d60465c
Pratul Makar <pratulmakar02@gmail.com>
2024-08-29 12:32:27 +0530

    new

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -143,7 +143,7 @@ export const Navigation = () => {
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar h-full bg-zinc-800 overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          "group/sidebar h-full dark:bg-zinc-800 overflow-y-auto relative flex w-60 flex-col z-[99999]",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0"
         )}
